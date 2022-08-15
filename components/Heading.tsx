03ed37f8aae9379792e866d8b55612e8dc7ceab9
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-08-15 09:51:40 +0530

    initial commit

---
+++ components/Heading.tsx
@@ -0,0 +1,4 @@
+const Heading = ({ children }: { children: React.ReactNode }) => {
+  return <h6 className="font-medium text-zinc-100">{children}</h6>;
+};
+export default Heading;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="max-w-screen-md mx-auto px-8 py-8">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
