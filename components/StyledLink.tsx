75d70d0919cca7f05f88a724f4173127b2249193
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-08-22 08:22:29 +0530

    fixed styled link

--- components/StyledLink.tsx
+++ components/StyledLink.tsx
@@ -3,19 +3,22 @@ import Link, { LinkProps } from "next/link";
 import Tooltip from "./Tooltip";
 
 type Props = {
+  href: string;
   children: React.ReactNode;
-} & LinkProps;
-
-const StyledLink = ({ children, ...props }: Props) => {
+};
+const StyledLink = ({ href, children }: Props) => {
   return (
-    <Link {...props}>
-      <Tooltip content="Visit link" className="mt-2">
-        <a className="underline underline-offset-[8px] cursor-pointer decoration-zinc-600 hover:decoration-zinc-400 italic font-medium text-zinc-300 hover:text-white duration-300 ease-out font-sora mx-1">
-          {" "}
-          {children}{" "}
-        </a>
-      </Tooltip>
-    </Link>
+    <Tooltip content="Visit link" className="mt-2">
+      <a
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+        className="underline underline-offset-[8px] cursor-pointer decoration-zinc-600 hover:decoration-zinc-400 italic font-medium text-zinc-300 hover:text-white duration-300 ease-out font-sora mx-1"
+      >
+        {" "}
+        {children}{" "}
+      </a>
+    </Tooltip>
   );
 };
 
